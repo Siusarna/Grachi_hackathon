ec4dce525acbeab62cf0d83ed4bf48690ed5ce8a
Siusarna <42269105+Siusarna@users.noreply.github.com>
2020-02-22 19:32:44 +0200

    add function for builded page

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,5 +1,10 @@
 const textRequests = require('./textRequests/textRequest');
+const path = require('path');
 
 module.exports = app => {
   textRequests(app);
+  app.get('/', (req, res) => {
+    console.log(path.join(__dirname, '..', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+  });
 };
